fd50355214fa96802ce1b4dc351fe1800d796f4d
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-15 13:18:41 +0200

    add user.js-MCR

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,64 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
                    CAPSTONE-BLOGAPI
 ------------------------------------------------------- */
-const {mongoose} = require("../configs/dbConnection")
+const { mongoose } = require("../configs/dbConnection");
 
-const UserSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema(
+  {
     username: {
-        
-    }
-})
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ src/routes/user.js
@@ -0,0 +1,16 @@
+"use strict";
+/* -------------------------------------------------------
+                   CAPSTONE-BLOGAPI
+------------------------------------------------------- */
+const router = require("express").Router();
+
+const { list, read, update, deletee } = require("../controllers/user");
+
+// URL: /users
+
+router.route("/").get(list);
+
+router.route("/:id").get(read).put(update).patch(update).delete(deletee);
+
+/* ------------------------------------------------------- */
+module.exports = router;
